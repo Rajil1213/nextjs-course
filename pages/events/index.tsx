abb446a048e5d647de5ca13b581793780c0119c9
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-15 12:27:21 +0545

    Add search to all events page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
+
+import { useRouter } from "next/router";
+
+import EventsList from "@/components/events/eventList";
+import EventsSearch from "@/components/events/eventsSearch";
+import { getAllEvents } from "@/data/dummy-data";
 
 const Events = () => {
+  const allEvents = getAllEvents();
+  const router = useRouter();
+
+  const eventsSearchHandler = (year: string, month: string) => {
+    const filteredEventsPath = `/events/${year}/${month}`;
+    router.push(filteredEventsPath);
+  };
+
   return (
-    <div>
-      <h1>EventsPage</h1>
-    </div>
+    <>
+      <EventsSearch onSearch={eventsSearchHandler} />
+      <EventsList events={allEvents} />
+    </>
   );
 };
 
