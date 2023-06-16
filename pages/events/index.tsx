a8463714aee0214f49e47c788d57f449d7e31d5e
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-16 15:16:44 +0545

    Use SSG for all events page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 
+import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
 
 import EventsList from "@/components/events/eventList";
 import EventsSearch from "@/components/events/eventsSearch";
-import { getAllEvents } from "@/data/dummy-data";
+import { EventsResponseModel, getAllEvents } from "@/services/events";
 
-const Events = () => {
-  const allEvents = getAllEvents();
+interface EventsProps {
+  events?: Array<EventsResponseModel>;
+}
+
+const Events: React.FC<EventsProps> = (props) => {
   const router = useRouter();
+
+  if (!props.events) {
+    return <p className="center">Loading...</p>;
+  }
 
   const eventsSearchHandler = (year: string, month: string) => {
     const filteredEventsPath = `/events/${year}/${month}`;
@@ -18,9 +26,20 @@ const Events = () => {
   return (
     <>
       <EventsSearch onSearch={eventsSearchHandler} />
-      <EventsList events={allEvents} />
+      <EventsList events={props.events} />
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps<EventsProps> = async () => {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events
+    },
+    revalidate: 10
+  };
 };
 
 export default Events;
