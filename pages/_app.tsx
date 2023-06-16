0feea798844bdd0a53a8fe4a0f84e706c123933e
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-16 16:19:57 +0545

    Add head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import Layout from "@/components/layout/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Find the most exciting events near you" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
