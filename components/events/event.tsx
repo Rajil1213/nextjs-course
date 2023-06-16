74e36d2e2a76265a44e7533c8008e88dea39f752
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-16 12:57:55 +0545

    Use zod type

--- components/events/event.tsx
+++ components/events/event.tsx
@@ -1,24 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 
+import { EventsResponseModel } from "@/services/events";
+
 import AddressIcon from "../icons/addressIcon";
 import ArrowRightIcon from "../icons/arrowRightIcon";
 import DateIcon from "../icons/dateIcon";
 import Button from "../ui/button";
 import classes from "./event.module.css";
 
-export interface EventInfo {
-  id: string;
-  title: string;
-  description: string;
-  location: string;
-  date: string;
-  image: string;
-  isFeatured: boolean;
-}
-
 interface EventProps {
-  event: EventInfo;
+  event: EventsResponseModel;
 }
 
 const Event: React.FC<EventProps> = ({ event }) => {
