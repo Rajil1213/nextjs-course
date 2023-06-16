539f5dac08510f474da161eab9180d68d57c5e1b
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-16 12:58:14 +0545

    Add staticProps to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,31 @@
-import { Inter } from 'next/font/google';
+import { GetStaticProps } from "next";
+import { Inter } from "next/font/google";
 
-import EventsList from '@/components/events/eventList';
-import { getFeaturedEvents } from '@/data/dummy-data';
+import EventsList from "@/components/events/eventList";
+import { EventsResponseModel, getAllEvents } from "@/services/events";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  const featuredEvents = getFeaturedEvents();
+interface HomePageProps {
+  featuredEvents: Array<EventsResponseModel>;
+}
 
+export default function Home(props: HomePageProps) {
   return (
     <div>
-      <EventsList events={featuredEvents} />
+      <EventsList events={props.featuredEvents} />
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+  const events = await getAllEvents();
+  const featuredEvents = events.filter((event) => event.isFeatured);
+
+  return {
+    props: {
+      featuredEvents
+    },
+    revalidate: 20
+  };
+};
