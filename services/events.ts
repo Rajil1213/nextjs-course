43559b80c79fe3f49dab6bd752b848563537a9e6
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-16 12:57:49 +0545

    Create service to fetch all events and add its types

---
+++ services/events.ts
@@ -0,0 +1,52 @@
+import dotenv from "dotenv";
+import { z } from "zod";
+
+// load config from .env
+dotenv.config();
+
+export const EventResponse = z.object({
+  id: z
+    .string({ required_error: "id is required" })
+    .trim()
+    .min(2, "id must be at least two characters")
+    .startsWith("e", "id must start with e"),
+  title: z.string({ required_error: "title is required" }).trim().min(1, "title cannot be empty"),
+  description: z.string({ required_error: "description is required" }),
+  image: z
+    .string({ required_error: "image is required" })
+    .trim()
+    .startsWith("images", "path to images must begin with `images`"),
+  location: z
+    .string({ required_error: "location is required" })
+    .trim()
+    .min(1, "location cannot be empty"),
+  date: z
+    .string({ required_error: "date is required" })
+    .trim()
+    .min(1, "date cannot be empty")
+    .regex(/\d{4}\-\d{2}\-\d{2}/, "date should be in YYYY-MM-DD format"),
+  isFeatured: z.boolean()
+});
+
+export type EventsResponseModel = z.infer<typeof EventResponse>;
+
+export const getAllEvents = async (): Promise<Array<EventsResponseModel>> => {
+  const uri = `${process.env.FIREBASE_URI}/events.json`;
+  console.log(uri);
+  const resp = await fetch(uri);
+  const data = await resp.json();
+
+  const respEvents: Array<EventsResponseModel> = [];
+
+  // transform data
+  for (const key in data) {
+    respEvents.push({
+      id: key,
+      ...data[key]
+    });
+  }
+
+  // validate against zod schema
+  const events = z.array(EventResponse).parse(respEvents);
+  return events;
+};
