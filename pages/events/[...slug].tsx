33b17ab46fa1bd83deabb81b3ae7a2a8afffa36d
Rajil Bajracharya <imrb1213@gmail.com>
2023-06-16 15:17:37 +0545

    Add ServerSideProps to slug page

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,20 +1,26 @@
 import React from "react";
 
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 
 import EventsList from "@/components/events/eventList";
 import ResultsTitle from "@/components/events/resultsTitle";
 import Button from "@/components/ui/button";
 import ErrorAlert from "@/components/ui/errorAlert";
-import { getFilteredEvents } from "@/data/dummy-data";
+import { EventsResponseModel, getFilteredEvents } from "@/services/events";
 
-const FilteredEvents = () => {
+type FilteredEventsProps =
+  | {
+      filteredEvents: Array<EventsResponseModel>;
+      hasError: false;
+    }
+  | {
+      filteredEvents: Record<string, never>;
+      hasError: true;
+    };
+
+const FilteredEvents: React.FC<FilteredEventsProps> = (props) => {
   const router = useRouter();
-  const filterData = router.query.slug;
-
-  if (!filterData) {
-    return <p className="center">Loading...</p>;
-  }
 
   const invalidFilter = (
     <div className="center">
@@ -25,24 +31,43 @@ const FilteredEvents = () => {
     </div>
   );
 
-  if (!Array.isArray(filterData)) return invalidFilter;
-  if (filterData.length !== 2) return invalidFilter;
-
-  const [year, month] = filterData;
-  const numYear = +year;
-  const numMonth = +month;
-
   // invalid cases
-  if (isNaN(numYear) || isNaN(numMonth) || numMonth > 12 || numMonth < 1) return invalidFilter;
-
-  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+  if (props.hasError) return invalidFilter;
 
   return (
     <>
-      <ResultsTitle date={`${year}/${month}`} />
-      <EventsList events={filteredEvents} />
+      <ResultsTitle date={`${router.query?.slug}`} />
+      <EventsList events={props.filteredEvents} />
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<FilteredEventsProps> = async (context) => {
+  const slug = context.params?.slug;
+  const errorProps: { props: FilteredEventsProps } = {
+    props: {
+      filteredEvents: {},
+      hasError: true
+    }
+  };
+
+  if (!slug || !Array.isArray(slug) || slug.length !== 2) {
+    return errorProps;
+  }
+
+  const [year, month] = slug;
+  const numYear = +year;
+  const numMonth = +month;
+
+  if (isNaN(numYear) || isNaN(numMonth)) return errorProps;
+
+  const filteredEvents = await getFilteredEvents(numYear, numMonth);
+  return {
+    props: {
+      filteredEvents,
+      hasError: false
+    }
+  };
 };
 
 export default FilteredEvents;
